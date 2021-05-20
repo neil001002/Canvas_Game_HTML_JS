25424a9ec7a5783ea6d71dd739d2579379928cd3
Sunil Kumar <neil001002@gmail.com>
2021-05-20 14:38:09 +0530

    Remove off screen projectiles

--- index.js
+++ index.js
@@ -108,8 +108,20 @@ function animate() {
   animationId = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height); //to draw particles instead of lines
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    //remove off screen projectiles from game
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
 
   enemies.forEach((enemy, index) => {
@@ -139,6 +151,7 @@ function animate() {
 
 // Projectiles call
 addEventListener("click", (event) => {
+  console.log(projectiles);
   const angle = Math.atan2(
     event.clientY - canvas.height / 2,
     event.clientX - canvas.width / 2

1fefb3e84aa6a73206b88161c0aa153e65500d45
Sunil Kumar <neil001002@gmail.com>
2021-05-20 16:00:27 +0530

    Create particle explosion on hit

--- index.js
+++ index.js
@@ -68,6 +68,37 @@ class Enemy {
   }
 }
 
+const friction = 0.99;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
@@ -76,6 +107,7 @@ const player = new Player(x, y, 10, "white");
 
 const projectiles = [];
 const enemies = [];
+const particles = [];
 
 // Enemies call
 function spawnEnemies() {
@@ -109,6 +141,14 @@ function animate() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height); //to draw particles instead of lines
   player.draw();
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
+
   projectiles.forEach((projectile, index) => {
     projectile.update();
 
@@ -141,6 +181,21 @@ function animate() {
 
       // when projectile touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
+        //particles call create explosion
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6),
+              }
+            )
+          );
+        }
         if (enemy.radius - 10 > 10) {
           gsap.to(enemy, {
             //animate shrinking
